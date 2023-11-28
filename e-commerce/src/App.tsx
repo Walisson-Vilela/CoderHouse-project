dd9d465d870219ea5b3ca6512f57d6a669ce47a1
Vilela Walisson <QG6251@engie.com>
2023-11-28 11:13:05 -0300

    Adição de mapeamento de liste de itens
    Adição de mapeamento de liste de itens

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { Header } from './components/Header'
 import './App.css'
+import { Hero } from './components/Hero'
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
+      <Hero />
     </div>
   )
 }
---
+++ e-commerce/src/components/Hero/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Hero = () => {
+
+    const title = ['Nome', 'adadasf', 'adadasf']
+    return (
+
+        <>
+            {title.map(item => (
+                <React.Fragment key={item}>
+                    <h1>{item}</h1>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adpsicing elit. Ex molestiae voluptate return</p>
+                    </div>
+                </React.Fragment>
+            ))}
+
+        </>
+    )
+}
+
+export { Hero }
