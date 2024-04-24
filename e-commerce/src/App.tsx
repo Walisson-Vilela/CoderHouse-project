08cb59757d12819a2d440446e835c5b276af5b04
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-04-23 21:38:27 -0300

    [STYLE] = Atualizacao no estilo
    Atualizacao no estilo

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header'
 import './App.css'
+import { Hero } from './components/Hero'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
+      <Hero />
 
     </div>
   )
