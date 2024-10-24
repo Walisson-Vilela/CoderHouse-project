db379452374d3314ab37dbd82def18dd6d1b6110
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-24 10:16:48 -0300

    code adjustments
    code adjustments

--- e-commerce/src/components/Header/Header.tsx
+++ e-commerce/src/components/Header/Header.tsx
@@ -1,23 +1,19 @@
-import './header.module.css';
+import "./header.module.css";
 
 const Header = () => {
-    return (
-
-        <nav className='w-full flex justify-between items-center p-5 bg-white shadow-xl'>
-            <span>Nome da Compania</span>
-            <ul style={{ display: 'flex' }}>
-                <a>Features</a>
-                <li>Premium</li>
-                <li>Suporte</li>
-                <li>
-                    <button>
-                        Cadastro
-                    </button>
-                </li>
-            </ul >
-        </nav >
-
-    );
-}
+  return (
+    <nav className="w-full flex justify-between items-center p-5 bg-white shadow-md">
+      <span>Nome da Compania</span>
+      <ul style={{ display: "flex" }}>
+        <a>Features</a>
+        <li>Premium</li>
+        <li>Suporte</li>
+        <li>
+          <button className="rounded-none bg-customColor rounded-md text-white px-6 py-3">Cadastro</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export { Header };
