ad0d1cd00d152fcaa10c35b1a7274644fe911c89
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-25 14:41:18 -0300

    Layout adjustments
    Layout adjustments

--- e-commerce/src/App/Header/CartWidget.tsx
+++ e-commerce/src/App/Header/CartWidget.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import CartWidgetImg from "../../images/cartWidget.svg";
+import CartWidgetImg from "../../images/trunk.png";
 
 const CarWidget = () => {
   return (
     <div className="mx-2">
-      <img src={CartWidgetImg} alt="Dogs Icon" />
+      <img src={CartWidgetImg} className="w-12" alt="Dogs Icon" />
     </div>
   );
 };
--- e-commerce/src/App/Header/Navbar.tsx
+++ e-commerce/src/App/Header/Navbar.tsx
@@ -1,17 +1,28 @@
 import CartWidget from "./CartWidget";
+import BrazilFlagImg from "../../images/brazilFlag.png";
+import UKFlagImg from "../../images/ukFlag.png";
 
 interface Iprops {
-    name: string;
+  name: string;
 }
 
-const Header = ({name}: Iprops) => {
+const Header = ({ name }: Iprops) => {
   return (
     <nav className="w-full flex justify-between items-center p-5 bg-white shadow-md">
-      <span>Empresa de {name}</span>
-      <ul className="flex flex-row items-center">
+      <div className="w-3/12 flex flex-row items-center">
+        <span className="mr-6">Empresa de {name}</span>
+        <div className="mx-2">
+          <img src={BrazilFlagImg} className="w-8 cursor-pointer " alt="Brazil flag Icon" />
+        </div>
+        <div className="mx-2">
+          <img src={UKFlagImg} className="w-8 cursor-pointer" alt="UK Flag Icon" />
+        </div>
+      </div>
+      <ul className="flex flex-row items-center"> 
         <li className="mx-2">Armas</li>
         <li className="mx-2">Poções</li>
         <li className="mx-2">Livros</li>
+        <li className="mx-2">Cancelar</li>
         <CartWidget />
       </ul>
     </nav>
