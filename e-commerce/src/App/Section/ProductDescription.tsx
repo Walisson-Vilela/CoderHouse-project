72c89489901c80563323273770af232da3dac9f2
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-26 16:25:02 -0300

    Field height adjustments description
    Field height adjustments description

--- e-commerce/src/App/Section/ProductDescription.tsx
+++ e-commerce/src/App/Section/ProductDescription.tsx
@@ -4,8 +4,9 @@ interface IProps {
 
 const ProductDescription = ({ description }: IProps) => {
   return (
-    <section className="col-span-3 relative min-h-24 border-4 border-white rounded-md items-center p-5 bg-gradient-to-t from-blue-950/60 to-blue-300/40 shadow-md">
-      Image adjustments{description}
+    <section className="col-span-3 relative h-32 border-4 border-white rounded-md items-center p-5 bg-gradient-to-t from-blue-950/60 to-blue-300/40 shadow-md">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard{description}
     </section>
   );
 };
