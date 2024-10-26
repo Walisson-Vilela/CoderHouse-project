87d67b717de7f72d2c25e5c0463dbe2eb1a1465a
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-26 16:19:26 -0300

    Image adjustments
    Image adjustments

--- e-commerce/src/App/App.tsx
+++ e-commerce/src/App/App.tsx
@@ -2,15 +2,24 @@ import { Header } from "./Header/Navbar";
 import ItemListContainer from "./Section/ItemListContainer";
 import ProductDescription from "./Section/ProductDescription";
 import PurchaseSummary from "./Section/PurchaseSummary";
+import PersonaImg from "../images/persona1.png";
 
 function App() {
   return (
-    <div className="w-screen p-2 h-screen ">
-      <div className="grid grid-cols-3 gap-4 ">
-        <Header/>
+    <div className="w-screen p-2 h-screen">
+      <div className="grid grid-cols-3 gap-4 relative">
+        <Header />
         <ProductDescription />
-        <img src="#" />
-        <div className="grid col-span-2 gap-4 h-full">
+
+        <div
+          className=" col-span-1 -mx-8"
+          style={{ backgroundImage: `url(${PersonaImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover' }}
+        >
+        </div>
+
+        <div className="grid col-span-2 gap-4 h-full z-10 relative">
           <ItemListContainer />
           <PurchaseSummary />
         </div>
