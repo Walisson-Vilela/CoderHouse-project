fa903ee5d82a56b02e9202b715bcabbaae1d9eee
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-26 12:52:25 -0300

    layout adjustments
    layout adjustments

--- e-commerce/src/App/App.tsx
+++ e-commerce/src/App/App.tsx
@@ -5,12 +5,12 @@ import PurchaseSummary from "./Section/PurchaseSummary";
 
 function App() {
   return (
-    <div className="w-screen p-2 ">
-      <div class="grid grid-cols-3 gap-4">
+    <div className="w-screen p-2 h-screen ">
+      <div className="grid grid-cols-3 gap-4">
         <Header/>
         <ProductDescription />
         <img src="#" />
-        <div className="grid col-span-2 gap-4">
+        <div className="grid col-span-2 gap-4 h-full">
           <ItemListContainer />
           <PurchaseSummary />
         </div>
