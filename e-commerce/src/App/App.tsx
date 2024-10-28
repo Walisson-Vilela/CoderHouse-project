90a42ce67783ea34ecd1f46022b4f9f1f95efc79
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-28 17:27:21 -0300

    layout adjustments
    layout adjustments

--- e-commerce/src/App/App.tsx
+++ e-commerce/src/App/App.tsx
@@ -1,14 +1,14 @@
 import { Header } from "./Header/Navbar";
 import ItemCount from "./Section/ItemCount";
 import ItemListContainer from "./Section/ItemListContainer";
+import Section from "./Section/Section";
 
 function App() {
   
   return (
     <div className="w-screen">
       <Header name={"Walisson"} />
-      <ItemListContainer />
-      <ItemCount />
+      <Section />
   
     </div>
   );
---
+++ e-commerce/src/App/Section/Section.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ItemListContainer from './ItemListContainer'
+import ItemCount from './ItemCount'
+
+const Section = () => {
+  return (
+    <div className='p-4'>
+        <ItemListContainer />
+        <ItemCount />
+    </div>
+  )
+}
+
+export default Section
