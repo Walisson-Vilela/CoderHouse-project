42948859f820003dac99473a79b5032e75ae6e9f
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-28 11:34:34 -0300

    Adjustments to the button color when disabled
    Adjustments to the button color when disabled

--- e-commerce/src/components/Button.tsx
+++ e-commerce/src/components/Button.tsx
@@ -11,7 +11,11 @@ const Button = ({ name, onClick, disabled }: Iprops) => {
     <button
       onClick={onClick}
       disabled={disabled}
-      className="h-9 items-center flex border-2 border-t-white border-indigo-400 rounded-md px-2 bg-gradient-to-t from-blue-800 to-blue-300 shadow-md "
+      className={`h-9 items-center flex border-2 border-t-white rounded-md px-2 shadow-md ${
+        disabled
+          ? "bg-gray-400 cursor-not-allowed" // Cor e estilo quando desabilitado
+          : "bg-gradient-to-t from-blue-800 to-blue-300 border-indigo-400"
+      }`}
     >
       {name}
     </button>
