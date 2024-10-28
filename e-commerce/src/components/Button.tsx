f77379afa2ee65ee90473e86c0a05f3dfeeb88d8
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-28 11:31:03 -0300

    Adjustment in item count component
    Adjustment in item count component

--- e-commerce/src/components/Button.tsx
+++ e-commerce/src/components/Button.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 interface Iprops {
-    name: string;
+  name: string;
+  onClick: () => void;
+  disabled: boolean;
 }
 
-const Button: React.FC<Iprops> = ({name}) => {
+const Button = ({ name, onClick, disabled }: Iprops) => {
   return (
-    <button>{name}</button>
-  )
-}
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className="h-9 items-center flex border-2 border-t-white border-indigo-400 rounded-md px-2 bg-gradient-to-t from-blue-800 to-blue-300 shadow-md "
+    >
+      {name}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
