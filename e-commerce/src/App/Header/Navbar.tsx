dad159ee272434fd2d42eabda909e20193ab409f
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-28 15:58:54 -0300

    criacao de lista de itens
    criacao de lista de itens

--- e-commerce/src/App/Header/Navbar.tsx
+++ e-commerce/src/App/Header/Navbar.tsx
@@ -1,22 +1,14 @@
 import CartWidget from "./CartWidget";
 
 interface Iprops {
-    name: string;
+  name: string;
 }
 
-const Header = ({name}: Iprops) => {
+const Header = ({ name }: Iprops) => {
   return (
     <nav className="w-full flex justify-between items-center p-5 bg-white shadow-md">
       <span>Empresa de {name}</span>
-      <ul className="flex flex-row items-center">
-        <li className="mx-2">Features</li>
-        <li className="mx-2">Premium</li>
-        <li className="mx-2">Suporte</li>
-        <li >
-          <button className="mx-2 bg-primaryColor rounded-md outline outline-cyan-500 text-white px-3 py-1">
-            Cadastro
-          </button>
-        </li>
+      <ul className="flex flex-row items-center"> 
         <CartWidget />
       </ul>
     </nav>
