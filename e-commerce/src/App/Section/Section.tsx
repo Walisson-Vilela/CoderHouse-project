94f471d24135d52b07072a66b503bb98770bf472
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-11-02 18:22:16 -0300

    Criado component Card
    Criado component Card

--- e-commerce/src/App/Section/Section.tsx
+++ e-commerce/src/App/Section/Section.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ItemListContainer from './ItemListContainer'
 import ItemCount from './ItemCount'
+import Card from './Card'
+import Bob from '../../assets/bob_esponja.jpg';
+
 
 const Section = () => {
+const[name,setName] = useState<string>("Walisson")
+
   return (
     <div className='p-4'>
         <ItemListContainer />
+        <Card image={Bob} />
         <ItemCount />
     </div>
   )
