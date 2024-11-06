1fef1d009002f30bdb918ecf000cf14951231a8f
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-11-06 15:21:28 -0300

    consumo de api de pokemon
    consumo de api de pokemon

--- e-commerce/src/components/Card.tsx
+++ e-commerce/src/components/Card.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
-import { get } from "../services/api.services";
+import { get, post } from "../services/api.services";
 import { Loading } from "./Loading";
 
 const Card = () => {
   useEffect(() => {
     const onMount = async () => {
-      const result = await get("/character/?page=2");
+      const result = await get("pokemon/pikachu");
+      // const data = { 
+      //   nome: 'Walisson',
+      //   password: '12345',
+      //   email: 'walisson.vilela@hotmail.com'
+      // }
+      // const result = await post("/character", data);
       console.log(result);
     };
     onMount();
--- e-commerce/src/services/api.services.ts
+++ e-commerce/src/services/api.services.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-console.log(BASE_URL); 
+console.log(BASE_URL);
 
 export async function get(url) {
   return axios.get(BASE_URL + url);
 }
-export async function post(url) {
-  return axios.post(BASE_URL + url);
+
+export async function post(url, data) {
+  return axios.post(BASE_URL + url, data, {
+    headers: {
+      Token: "walisson.vilela@hotmail.com",
+    },
+  });
 }
